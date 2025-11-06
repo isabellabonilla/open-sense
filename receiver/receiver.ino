48ceba96af376901c31360b097054c337efe06aa
Andrew Perez-Ledo <104042824+andrewperezledo@users.noreply.github.com>
2025-11-06 16:59:42 -0500

    Added ESP32 Arduino Code via File Upload

---
+++ receiver/receiver.ino
@@ -0,0 +1,212 @@
+#include <SPI.h>
+#include <RH_RF95.h>
+#include <WiFi.h>
+#include <Firebase_ESP_Client.h>
+#include "addons/TokenHelper.h"
+#include "addons/RTDBHelper.h"
+
+#define RF95_FREQ 915.0
+#define LED       2
+#define RFM95_CS 5
+#define RFM95_RST 4
+#define RFM95_INT 14
+
+// Arduino9x_RX
+// -*- mode: C++ -*-
+// Example sketch showing how to create a simple messaging client (receiver)
+// with the RH_RF95 class. RH_RF95 class does not provide for addressing or
+// reliability, so you should only use RH_RF95 if you do not need the higher
+// level messaging abilities.
+// It is designed to work with the other example Arduino9x_TX
+
+
+// Fire Base stuff
+#define API_KEY "AIzaSyAp1IwI5lIyzyRm1Uv9MAPkQ3x43SGksWM"
+#define DATABASE_URL "https://opensense-b7275-default-rtdb.firebaseio.com"
+
+// const char *ssid = "1d-103@Lexington_crossing";
+// const char *password = "597NUUDU5";
+const char *ssid = "Iphone 3G";
+const char *password = "password";
+
+
+FirebaseData fbdo;
+FirebaseAuth auth;
+FirebaseConfig config;
+
+unsigned long sendDataPrevMillis = 0;
+bool signupOK = false;
+
+// Singleton instance of the radio driver
+RH_RF95 rf95(RFM95_CS, RFM95_INT);
+
+// Blinky on receipt
+#define LED 2
+struct SensorData {
+  float f;        // e.g., 4 bytes temp in farenheight
+  float h;           // e.g., 4 bytes humidity as percentage
+  uint16_t pm25_value;      // e.g., 2 bytes airquality 
+  uint16_t gasses;           // e.g., 2 bytes gas sensor
+  uint16_t noise;            // 2 bytes
+};
+const size_t STRUCT_SIZE = sizeof(SensorData);
+
+void setup() 
+{
+  pinMode(LED, OUTPUT);     
+  pinMode(RFM95_RST, OUTPUT);
+  digitalWrite(RFM95_RST, HIGH);
+
+  Serial.begin(9600);
+  delay(100);
+
+  Serial.println("Arduino LoRa RX Test!");
+  
+  // manual reset
+  digitalWrite(RFM95_RST, LOW);
+  delay(10);
+  digitalWrite(RFM95_RST, HIGH);
+  delay(10);
+
+  while (!rf95.init()) {
+    Serial.println("LoRa radio init failed");
+    while (1);
+  }
+  Serial.println("LoRa radio init OK!");
+
+  // Defaults after init are 434.0MHz, modulation GFSK_Rb250Fd250, +13dbM
+  if (!rf95.setFrequency(RF95_FREQ)) {
+    Serial.println("setFrequency failed");
+    while (1);
+  }
+  Serial.print("Set Freq to: "); Serial.println(RF95_FREQ);
+
+  // Defaults after init are 434.0MHz, 13dBm, Bw = 125 kHz, Cr = 4/5, Sf = 128chips/symbol, CRC on
+
+  // The default transmitter power is 13dBm, using PA_BOOST.
+  // If you are using RFM95/96/97/98 modules which uses the PA_BOOST transmitter pin, then 
+  // you can set transmitter powers from 5 to 23 dBm:
+  rf95.setTxPower(23, false);
+
+  // Conncting to wifi
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected.");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  config.api_key = API_KEY;
+  config.database_url = DATABASE_URL;
+  if (Firebase.signUp(&config, &auth, "", "")){
+    Serial.println("Signup OK");
+    signupOK = true;
+  }
+  else{
+    Serial.printf("%s\n", config.signer.signupError.message.c_str());
+  }
+  
+  config.token_status_callback = tokenStatusCallback;
+  Firebase.begin(&config, &auth);
+  Firebase.reconnectWiFi(true);
+}
+
+void loop()
+{
+  if (rf95.available())
+  {
+    // Should be a message for us now   
+    uint8_t buf[STRUCT_SIZE];
+    uint8_t len = STRUCT_SIZE;
+    
+    if (rf95.recv(buf, &len))
+    {
+      digitalWrite(LED, HIGH);
+      if (len == STRUCT_SIZE){
+        SensorData *receivedData = (SensorData *)buf;
+        Serial.println("\n-------------------------------------");
+        Serial.println("✅ Sensor Data Received!");
+        
+        // 4. Access and print the data using the -> operator
+        Serial.print("Temperature (F): ");
+        Serial.println(receivedData->f, 2); // Print float with 2 decimal places
+        
+        Serial.print("Humidity (%): ");
+        Serial.println(receivedData->h, 2);
+        
+        Serial.print("PM2.5 Value:     ");
+        Serial.println(receivedData->pm25_value);
+        
+        Serial.print("Gasses Value:    ");
+        Serial.println(receivedData->gasses);
+        
+        Serial.print("Noise Value:     ");
+        Serial.println(receivedData->noise);
+        
+        Serial.println("-------------------------------------");
+
+        // SENDING TO FIREBASE
+        Serial.println("Sending received data to Firebase");
+        if (Firebase.ready() & signupOK ){
+          // PM 2.5
+          if (Firebase.RTDB.setInt(&fbdo, "latest/pm", receivedData->pm25_value)){
+          Serial.println(); Serial.println(" PM sent to Firebase as Int.");
+          }
+          else Serial.println("Failed" + fbdo.errorReason());
+          //gas
+          if (Firebase.RTDB.setInt(&fbdo, "latest/gas", receivedData->gasses)){
+          Serial.println(); Serial.println(" Gasses sent to Firebase as Int.");
+          }
+          else Serial.println("Failed" + fbdo.errorReason());
+
+          // Noise
+          if (Firebase.RTDB.setInt(&fbdo, "latest/sound", receivedData->noise)){
+          Serial.println(); Serial.println(" PM sent to Firebase as Int.");
+          }
+          else Serial.println("Failed" + fbdo.errorReason());
+          // humidity 
+          if (Firebase.RTDB.setFloat(&fbdo, "latest/humidity", receivedData->h)){
+          Serial.println(); Serial.println("Humidity sent to Firebase as Float.");
+          }
+          else Serial.println("Failed" + fbdo.errorReason());
+          // temperature in farenheight
+          if (Firebase.RTDB.setFloat(&fbdo, "latest/temp", receivedData->f)){
+          Serial.println();  Serial.println("Temperature sent to Firebase as Float.");
+          }
+          else Serial.println("Failed" + fbdo.errorReason());
+
+        }
+      }
+      else{
+        Serial.print("\nError: Size Mismatch. Expected ");
+        Serial.print(STRUCT_SIZE);
+        Serial.print(" bytes, but received ");
+        Serial.print(len);
+        Serial.println(" bytes.");
+      }
+      RH_RF95::printBuffer("Received: ", buf, len);
+      Serial.print("Got: ");
+      Serial.println((char*)buf);
+       Serial.print("RSSI: ");
+      Serial.println(rf95.lastRssi(), DEC);
+      // Send a reply
+      uint8_t data[] = "And hello back to you";
+      rf95.send(data, sizeof(data));
+      rf95.waitPacketSent();
+      Serial.println("Sent a reply");
+      digitalWrite(LED, LOW);
+
+      
+    }
+
+    else
+    {
+      Serial.println("Receive failed");
+    }
+  }
+}
---
+++ sender/sender.ino
@@ -0,0 +1,164 @@
+#include <SPI.h>
+#include <RH_RF95.h>
+#include <SoftwareSerial.h>
+#include "Adafruit_PM25AQI.h"
+#include "DHT.h"
+
+
+#define LED       2
+#define RFM95_CS 5
+#define RFM95_RST 4
+#define RFM95_INT 14
+//must match RX's freq!
+#define RF95_FREQ 915.0
+#define DHTPIN 15
+#define DHTTYPE DHT22 // DHT 22 (AM2302)
+#define microphone 33
+#define gas 32
+#define gas_digital 13
+#define buzzer 0
+#define rx_pin 35
+
+// Singleton instance of the radio driver
+RH_RF95 rf95(RFM95_CS, RFM95_INT);
+
+SoftwareSerial pmSerial(rx_pin, 3);
+// initialize air quality sensor
+Adafruit_PM25AQI aqi = Adafruit_PM25AQI();
+// initialize temp/humid sensor
+DHT dht(DHTPIN, DHTTYPE);
+
+struct SensorData {
+  float f;        // e.g., 4 bytes temp in farenheight
+  float h;           // e.g., 4 bytes humidity as percentage
+  uint16_t pm25_value;      // e.g., 2 bytes airquality 
+  uint16_t gasses;           // e.g., 2 bytes gas sensor
+  uint16_t noise;            // 2 bytes
+};
+
+SensorData sensor_data;
+
+void setup() 
+{
+  dht.begin();
+  pinMode(RFM95_RST, OUTPUT);
+  pinMode(LED, OUTPUT);
+  pinMode(gas_digital, INPUT);
+  digitalWrite(RFM95_RST, HIGH);
+  pmSerial.begin(9600);
+
+  Serial.begin(115200);
+  delay(100);
+
+  Serial.println("Arduino LoRa TX Test!");
+
+  // manual reset
+  digitalWrite(RFM95_RST, LOW);
+  delay(10);
+  digitalWrite(RFM95_RST, HIGH);
+  delay(10);
+
+  while (!rf95.init()) {
+    Serial.println("LoRa radio init failed");
+    while (1);
+  }
+  Serial.println("LoRa radio init OK!");
+
+  // Defaults after init are 434.0MHz, modulation GFSK_Rb250Fd250, +13dbM
+  if (!rf95.setFrequency(RF95_FREQ)) {
+    Serial.println("setFrequency failed");
+    while (1);
+  }
+  Serial.print("Set Freq to: "); Serial.println(RF95_FREQ);
+  
+  rf95.setTxPower(23, false);
+
+  if (! aqi.begin_UART(&pmSerial)) { // connect to the sensor over software serial 
+    Serial.println("Could not find PM 2.5 sensor!");
+    while (1) delay(10);
+  }
+  Serial.println("PM25 found!");
+
+}
+
+int16_t packetnum = 0;  // packet counter, we increment per xmission
+
+void loop()
+{
+  // ************reading from PM air quality sensor**************
+  PM25_AQI_Data data;
+  if (! aqi.read(&data)) {
+    Serial.println("Could not read from AQI");
+    
+    delay(500);  // try again in a bit!
+    return;
+  }
+
+  Serial.println("AQI reading success");
+  sensor_data.pm25_value = data.aqi_pm25_us;
+  Serial.print(F("PM2.5 AQI US: ")); Serial.println(sensor_data.pm25_value);
+
+  // **************read temp/humid**********************
+  sensor_data.h = dht.readHumidity();
+  // Read temperature as Fahrenheit (isFahrenheit = true)
+  sensor_data.f = dht.readTemperature(true);
+  if (isnan(sensor_data.h) || isnan(sensor_data.f)) {
+    Serial.println(F("Failed to read from DHT sensor!"));
+    return;
+  }
+  Serial.print(F("Humidity: "));
+  Serial.print(sensor_data.h);
+  Serial.print(F("%  Temperature: "));
+  Serial.print(sensor_data.f);
+  Serial.println(F("°F"));
+
+  //  *************** Noise & Gas detection ****************
+  sensor_data.noise = analogRead(microphone);
+  Serial.print("Noise Level: "); Serial.println(sensor_data.noise);
+  sensor_data.gasses = analogRead(gas);
+  Serial.print("Gas Present/4095: "); Serial.println(sensor_data.gasses);
+  if (digitalRead(gas_digital) == HIGH){ 
+    digitalWrite(buzzer, HIGH);
+    Serial.println("Buzzing!");
+  }
+
+  // ******************* Lora Sending ************************
+  Serial.println("Sending to rf95_server");
+  // Send a message to rf95_server
+  
+  Serial.println("Sending Sensor Data Struct..."); 
+  
+  digitalWrite(LED, HIGH);
+  delay(10);
+  rf95.send( (uint8_t *)&sensor_data, sizeof(sensor_data));
+
+  Serial.println("Waiting for packet to complete..."); delay(10);
+  rf95.waitPacketSent();
+  // Now wait for a reply
+  uint8_t buf[RH_RF95_MAX_MESSAGE_LEN];
+  uint8_t len = sizeof(buf);
+
+  Serial.println("Waiting for reply..."); delay(100);
+  if (rf95.waitAvailableTimeout(1000))
+  { 
+    // Should be a reply message for us now   
+    if (rf95.recv(buf, &len))
+   {
+      Serial.print("Got reply: ");
+      Serial.println((char*)buf);
+      Serial.print("RSSI: ");
+      Serial.println(rf95.lastRssi(), DEC);    
+    }
+    else
+    {
+      Serial.println("Receive failed");
+    }
+  }
+  else
+  {
+    Serial.println("No reply, is there a listener around?");
+  }
+  delay(1000);
+  digitalWrite(LED, LOW); // message was sent
+
+}
